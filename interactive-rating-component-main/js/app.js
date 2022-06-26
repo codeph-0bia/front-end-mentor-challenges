455ae1cd4b01304c1adbbab6fe2d06c57eeac43f
Aman Gundhada <codephobia642@gmail.com>
2022-06-26 23:44:35 +0530

    handle rating buttons done

--- interactive-rating-component-main/js/app.js
+++ interactive-rating-component-main/js/app.js
@@ -3,19 +3,36 @@ const card_content_1 = document.querySelector(".card-content-1");
 const card_content_2 = document.querySelector(".card-content-2");
 
 const score_el = document.querySelector(".score");
-
 let score = 3;
 
+const ratingBtns = document.querySelectorAll('.rating-btn');
 
 
-//TODO: Submit btn
+
 submit_btn.addEventListener('click', onSubmit);
+ratingBtns.forEach(b => {
+    b.addEventListener('click', handleRatingClick);
+})
+
 
 function onSubmit() {
-    card_content_1.classList.add(".hide");
-    card_content_2.classList.remove(".hide");
+    card_content_1.classList.add("hide");
+    card_content_2.classList.remove("hide");
 
     score_el.textContent = score;
 }
 
-//TODO: HandleRatings buttons
+function handleRatingClick(event) {
+    ratingBtns.forEach(b => {
+        b.classList.remove(".active");
+    });
+
+    if (event.target.classList.contains("rating-btn")) {
+        event.target.classList.add("active");
+    } else {
+        event.target.parentElement.classList.add("active");
+    }
+
+    score = event.target.textContent;
+
+}
